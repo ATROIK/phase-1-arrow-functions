ecb9d71f7ae0f93e199faa4ae1061a300da9687a
andrewtroicki <atroicki@icloud.com>
2024-04-09 12:53:11 +1000

    tests complete

---
+++ index.js
@@ -0,0 +1,12 @@
+///function divide() {
+
+//}
+
+const divide = (param1, param2) => param1 / param2;
+divide(2000, 100);
+
+const square = (parameter1) => parameter1 * parameter1;
+square(100);
+
+const add = (number1, number2) => number1 + number2;
+add(5,5);
